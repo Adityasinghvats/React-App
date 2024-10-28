34515672251f8d5b273189f0ed6293da81f19d92
Adityasinghvats <131326798+Adityasinghvats@users.noreply.github.com>
2024-10-28 23:45:58 +0530

    Started vite+react app

---
+++ viteapp/src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Chai from './Chai.jsx'
+
+
+function App() {
+  return (
+    <>
+    <Chai />
+    <p>Welcome to vite app to learn about fragment</p>
+    </>
+  )
+}
+
+export default App

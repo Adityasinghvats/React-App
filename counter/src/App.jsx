2c0f9da0a10fb26000b50147c19f1d1feaefd5a4
Adityasinghvats <131326798+Adityasinghvats@users.noreply.github.com>
2024-10-30 19:44:13 +0530

    done with props

--- counter/src/App.jsx
+++ counter/src/App.jsx
@@ -1,30 +1,35 @@
 import { useState } from 'react'
 import './App.css'
+import Card from './components/Card'
 
 function App() {
-
   const [Counter, setCounter] = useState(0)
-
   // let Counter = 5
 
   const addValue = () => {
     console.log("Value added", Math.random())
     setCounter(Counter+1)
   }
-
   const decValue = () => {
     if(Counter>0){
       setCounter(Counter-1)
     }
   }
-
+  const obj = {
+    name:"Aditya",
+    job: "Software Developer"
+  }
+  const newArr = [1,2,3,5]
   return (
     <>
       <h1 className='bg-slate-400 p-4 rounded-lg mb-4 text-black'>Chai aur react</h1>
-      <h2 className='mb-4'>Counter value: {Counter}</h2>
+
+      {/* <h2 className='mb-4'>Counter value: {Counter}</h2>
       <button className='mb-4' onClick={addValue}>Add value</button>
       <br />
-      <button onClick={decValue}>Remove value</button>
+      <button onClick={decValue}>Remove value</button> */}
+
+      <Card channel="Chai aur code" Obj={obj} arr={newArr}/>
     </>
   )
 }

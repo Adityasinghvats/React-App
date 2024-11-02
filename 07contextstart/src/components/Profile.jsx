91571e9df5df2939595b9fbea58aadf6fc17a83a
Adityasinghvats <131326798+Adityasinghvats@users.noreply.github.com>
2024-11-02 21:42:31 +0530

    first project done with context

---
+++ 07contextstart/src/components/Profile.jsx
@@ -0,0 +1,18 @@
+import { useState, useContext } from "react";
+import UserContext from "../context/UserContext";
+
+function Profile(){
+    const {user} = useContext(UserContext)
+    if(!user){
+        return (
+            <div>Plase Login</div>
+        )
+    }
+    return(
+        <div>
+            Welcome {user.username}
+        </div>
+    )
+}
+
+export default Profile;
